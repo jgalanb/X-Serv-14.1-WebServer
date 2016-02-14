d04d1ff5ab8d6cd837d81456c33596d40a9d6d13
Jesús <jgalanb>
2016-02-14 15:01:22 +0100

    Entrega ejercicio 14.1 modificado por Jesus Galan Barba

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-"""
-Simple HTTP Server
-Jesus M. Gonzalez-Barahona and Gregorio Robles
-{jgb, grex} @ gsyc.es
-TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
-"""
+# Jesús Galán Barba
+# Ing. en Sistemas de Telecomunicaciones
 
 import socket
 
-# Create a TCP objet socket and bind it to a port
-# We bind to 'localhost', therefore only accepts connections from the
-# same machine
-# Port should be 80, but since it needs root privileges,
-# let's use one above 1024
+Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Socket.bind(('localhost', 2024))
 
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
-
-# Queue a maximum of 5 TCP connection requests
-
-mySocket.listen(5)
-
-# Accept connections, read incoming data, and answer back an HTML page
-#  (in an infinite loop)
+Socket.listen(1)
 
 while True:
-    print 'Waiting for connections'
-    (recvSocket, address) = mySocket.accept()
-    print 'HTTP request received:'
-    print recvSocket.recv(1024)
-    recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
-                    "\r\n")
-    recvSocket.close()
+	print 'Waiting for connections'
+	(recvSocket, address) = Socket.accept()
+	print 'HTTP request received:'
+	print recvSocket.recv(1024)
+	mensaje = "HTTP/1.1 200 OK\r\n\r\n" + "<html><body><h1>Hola! Eres de esta IP " + str(address[0]) +  " y de este puerto " + str(address[1]) +  "</h1></body></html>" + "\r\n"
+	recvSocket.send(mensaje)
+	recvSocket.close()
